9d87ceb28e6d3f301fb409a835a54d1f82a66658
Uwe Fladrich <uwe.fladrich@smhi.se>
2021-06-14 14:56:59 +0200

    Re-implement regular lat-lon grids

--- ocp_tool/grids/regular.py
+++ ocp_tool/grids/regular.py
@@ -3,49 +3,87 @@ import numpy as np
 from .gaussian import EARTH_RADIUS
 
 
+def _equidistant(start, end, N):
+    '''Divides the interval [start, end]  into N subintervals of equal size and
+    returns the midpoints of the subintervals'''
+    return np.linspace(start, end, 2*N+1)[1::2]
+
+
+def _interval_bounds(start, centers, end, *, loc, wrap=False):
+    center_midpoints = 0.5*(centers[:-1]+centers[1:])
+    if loc in ('l', 'left', 'lower'):
+        return np.array((start if not wrap else end, *center_midpoints))
+    elif loc in ('r', 'right', 'u', 'upper'):
+        return np.array((*center_midpoints, end if not wrap else start))
+    raise ValueError(f"Invalid value for 'loc' argument: {loc}")
+
+
+def _row_distribute(row, nrows):
+    return np.tile(row, (nrows, 1))
+
+
+def _col_distribute(col, ncols):
+    return np.tile(col, (ncols, 1)).T
+
+
 class RegularLatLonGrid:
 
-    def __init__(self, nlat, nlon):
-        self.nlat = nlat
-        self.nlon = nlon
+    def __init__(self, nlat, nlon, lats_start=-90):
+        self._OP = lats_start
+        self.lats = _equidistant(self._OP, -self._OP, nlat)
+        self.lons = _equidistant(0, 360, nlon)
+
+    @property
+    def nlat(self):
+        return len(self.lats)
+
+    @property
+    def nlon(self):
+        return len(self.lons)
 
     def cell_latitudes(self):
-        return np.tile(
-            np.linspace(90.0, -90.0, 2*self.nlat, endpoint=False)[1::2],
-            (self.nlon, 1)
-        ).T
+        return _col_distribute(self.lats, len(self.lons))
 
     def cell_longitudes(self):
-        return np.tile(
-            np.linspace(0, 360, self.nlon+1)[:-1],
-            (self.nlat, 1)
+        return _row_distribute(self.lons, len(self.lats))
+
+    def _cell_corner_latitudes(self):
+        upper_lats = _interval_bounds(self._OP, self.lats, -self._OP, loc='u')
+        lower_lats = _interval_bounds(self._OP, self.lats, -self._OP, loc='l')
+        return np.array(
+            [
+                _col_distribute(upper_lats, len(self.lons)),  # corner 1
+                _col_distribute(upper_lats, len(self.lons)),  # corner 2
+                _col_distribute(lower_lats, len(self.lons)),  # corner 3
+                _col_distribute(lower_lats, len(self.lons)),  # corner 4
+            ]
+        )
+
+    def _cell_corner_longitudes(self):
+        left_lons = _interval_bounds(0, self.lons, 360, loc='l')
+        right_lons = _interval_bounds(0, self.lons, 360, loc='r', wrap=True)
+        return np.array(
+            [
+                _row_distribute(right_lons, len(self.lats)),  # corner 1
+                _row_distribute(left_lons, len(self.lats)),  # corner 2
+                _row_distribute(left_lons, len(self.lats)),  # corner 3
+                _row_distribute(right_lons, len(self.lats)),  # corner 4
+            ]
         )
 
     def cell_corners(self):
-        border_lats = np.linspace(90.0, -90.0, self.nlat+1)
-        c_lat = np.empty((4, self.nlat, self.nlon))
-        c_lat[0, ...] = np.tile(border_lats[:-1], (self.nlon, 1)).T  # north
-        c_lat[1, ...] = np.tile(border_lats[:-1], (self.nlon, 1)).T  # north
-        c_lat[2, ...] = np.tile(border_lats[1:], (self.nlon, 1)).T  # south
-        c_lat[3, ...] = np.tile(border_lats[1:], (self.nlon, 1)).T  # south
-
-        border_lons = np.linspace(0, 360.0, 2*self.nlon+1)[1::2]
-        border_lons = np.insert(border_lons, 0, -border_lons[0])
-        c_lon = np.empty((4, self.nlat, self.nlon))
-        c_lon[0, ...] = np.tile(border_lons[1:], (self.nlat, 1))  # east
-        c_lon[1, ...] = np.tile(border_lons[:-1], (self.nlat, 1))  # west
-        c_lon[2, ...] = np.tile(border_lons[:-1], (self.nlat, 1))  # east
-        c_lon[3, ...] = np.tile(border_lons[1:], (self.nlat, 1))  # west
-
-        return np.array([c_lat, c_lon])
+        return np.array(
+            [self._cell_corner_latitudes(), self._cell_corner_longitudes()]
+        )
 
     def cell_areas(self):
-        sine = np.sin(
-            np.radians(
-                np.linspace(90.0, -90.0, self.nlat+1)
-            )
+        upper_lats = _interval_bounds(self._OP, self.lats, -self._OP, loc='u')
+        lower_lats = _interval_bounds(self._OP, self.lats, -self._OP, loc='l')
+        return _col_distribute(
+            2*np.pi*EARTH_RADIUS**2
+            * np.abs(
+                np.sin(np.radians(upper_lats))
+                - np.sin(np.radians(lower_lats))
+            )/len(self.lons),
+            len(self.lons)
         )
-        return np.tile(
-            2*np.pi*EARTH_RADIUS**2*(sine[:-1]-sine[1:]) / self.nlon,
-            (self.nlon, 1)
-        ).T
