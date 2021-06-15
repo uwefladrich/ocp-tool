12bb66ed77a098e8487a5488228285755359f4dd
Uwe Fladrich <uwe.fladrich@smhi.se>
2021-06-15 13:12:04 +0200

    Implement longitudes intevals centered around zero

--- ocp_tool/grids/regular.py
+++ ocp_tool/grids/regular.py
@@ -3,18 +3,32 @@ import numpy as np
 from .earth import RADIUS as EARTH_RADIUS
 
 
-def _equidistant(start, end, N):
-    '''Divides the interval [start, end]  into N subintervals of equal size and
-    returns the midpoints of the subintervals'''
-    return np.linspace(start, end, 2*N+1)[1::2]
+def _equidistant(start, end, N, *, first_at_start=False):
+    """Divides the interval [start, end]  into N subintervals of equal size and
+    returns the midpoints of the subintervals. If 'first_at_start' is true, the
+    first interval is centered around the start point."""
+    if first_at_start:
+        return np.linspace(start, end, N+1)[:-1]
+    else:
+        return np.linspace(start, end, 2*N+1)[1::2]
 
 
 def _interval_bounds(start, centers, end, *, loc, wrap=False):
+    """Returns the upper or lower bounds of [start, end] subintervals defined by
+    their centers. It is checked if the first center coincides with 'start' and
+    it is in that case assumed that the first interval is centered around the
+    starting point. If 'wrap' is True, the first lower bound (last upper) is
+    returned the same as the last upper (first lower)."""
     center_midpoints = 0.5*(centers[:-1]+centers[1:])
+    last_dx = 0.5*(end-centers[-1]) if np.isclose(start, centers[0]) else 0
     if loc in ('l', 'left', 'lower'):
-        return np.array((start if not wrap else end, *center_midpoints))
+        return np.array(
+            (end-last_dx if wrap else start-last_dx, *center_midpoints)
+        )
     elif loc in ('r', 'right', 'u', 'upper'):
-        return np.array((*center_midpoints, end if not wrap else start))
+        return np.array(
+            (*center_midpoints, start-last_dx if wrap else end-last_dx)
+        )
     raise ValueError(f"Invalid value for 'loc' argument: {loc}")
 
 
